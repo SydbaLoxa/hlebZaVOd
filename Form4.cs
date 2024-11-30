d467dd8a9fb40a70653f4875c2ffcbfc8c3d0c65
aaaannnnaaaaa <anya.strelkova21@gmail.com>
2024-11-30 13:39:22 +0400

    Update Form4.cs

--- Form4.cs
+++ Form4.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,16 +18,68 @@ namespace опд1
             InitializeComponent();
         }
 
+        string id;
+        string what_date;
+        string product_plan;
+        string product_real_quantity;
+        string connectionString = "Host=localhost;Username=postgres;Password=98321;Database=opd";
+        string queryEmployer = "SELECT * FROM for_employers";
+
+
+        private void Form4_Load(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM users";
+            string queryPlan = "SELECT * FROM plan";
+            using (var connection = new NpgsqlConnection(connectionString))
+            {
+                connection.Open();
+                var dataAdapter = new NpgsqlDataAdapter(query, connection);
+                var dataTable = new DataTable();
+
+
+                //connection.Open();
+                dataAdapter.Fill(dataTable);
+                dataGridView1.DataSource = dataTable;
+                //*********************************************************
+
+                var dataAdapter2 = new NpgsqlDataAdapter(queryEmployer, connection);
+                var dataTable2 = new DataTable();
+
+
+                //connection.Open();
+                dataAdapter2.Fill(dataTable2);
+                dataGridView2.DataSource = dataTable2;
+                //*********************************************************
+
+                var dataAdapter3 = new NpgsqlDataAdapter(queryPlan, connection);
+                var dataTable3 = new DataTable();
+
+
+                //connection.Open();
+                dataAdapter3.Fill(dataTable3);
+                dataGridView3.DataSource = dataTable3;
+            }
+        }
         private void exit_Click(object sender, EventArgs e)
         {
             //Form1 f1 = new Form1();
-            //this.Close();
+            this.Close();
             //f1.Show();
         }
 
         private void dataGridView2_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             //сюда пишем табличку for_employer
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView3_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
